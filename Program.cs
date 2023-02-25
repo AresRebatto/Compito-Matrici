e69167f1a4c220043d30fbf35f86ddd6b6c0cf81
AresRebatto <aresrebatt@gmail.com>
2023-02-25 12:46:37 +0100

    Aggiunta Determinante (Da fixare)

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 ﻿//Variabili
-int riga;
-int colonna;
+int rigaColonna;
 
 //Input
-Console.Write("Inserire il numero di righe: ");
-riga = Convert.ToInt32(Console.ReadLine());
+do
+{
+    Console.Write("Inserire il numero di righe-colonne: ");
+    rigaColonna = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Inserire il numero di colonne: ");
-colonna = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrice = new int[riga, colonna];
+    if(rigaColonna > 3)
+    {
+        Console.WriteLine("Valore non valido. Puoi inserire al massimo una matrice 3x3.");
+    }
+}while(rigaColonna > 3);
+
+
+int[,] matrice = new int[rigaColonna, rigaColonna];
 
 //Popolazione della matrice
-for(int i = 0; i < riga; i++)
+for(int i = 0; i < rigaColonna; i++)
 {
-    for(int n = 0; n < riga; n++)
+    for(int n = 0; n < rigaColonna; n++)
     {
         Console.Write($"Inserire il valore della {i+1}a riga e della {n+1}a colonna: ");
         matrice[i,n] = Convert.ToInt32(Console.ReadLine());
@@ -25,7 +31,8 @@ for(int i = 0; i < riga; i++)
 StampaMatrice(matrice);
 Console.WriteLine("\n");
 MatriceTrasposta(matrice);
-
+Console.WriteLine("\n");
+Console.WriteLine($"Il valore del determinante della matrice è {Determinante(matrice)}");
 
 //Metodi
 static void MatriceTrasposta(int[,] matrice)
@@ -53,4 +60,16 @@ static void StampaMatrice(int[,] matrice)
         }
         Console.Write("\n");
     }
+}
+
+static int Determinante(int[,] matrice)
+{
+    if(matrice.GetLength(0) == 3)
+    {
+        return matrice[0,0]*matrice[1,1]*matrice[2,2]+matrice[0,1]*matrice[1,1]*matrice[0,2]+matrice[0,2]*matrice[1,0]*matrice[2,1]-matrice[0,0]*matrice[1,2]*matrice[2,0]-matrice[0,1]*matrice[1,0]*matrice[2,2]-matrice[2,0]*matrice[1,1]*matrice[2,0];
+    }else
+    {
+        return matrice[0,0]*matrice[1,1]-matrice[0,1]*matrice[1,0];
+    }
+    
 }
